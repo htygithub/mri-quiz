17dc589ddc2451961233081229a5909f638f82fe
Lucas van Dijk <info@return1.net>
2014-06-14 20:52:29 +0200

    Added ignore file

--- mriquiz/urls.py
+++ mriquiz/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from quiz.views import QuizView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mriquiz.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', QuizView.as_view(), name='quiz_view')
 )
