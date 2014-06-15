dfe6af8ebabfc53f39c290fc71652438c681df73
Lucas van Dijk <info@return1.net>
2014-06-15 14:22:22 +0200

    Order by latin name

--- quiz/forms.py
+++ quiz/forms.py
@@ -4,7 +4,7 @@ from quiz.models import BrainStructure
 
 class QuizSubmitForm(forms.Form):
     answer = forms.ModelChoiceField(
-        queryset=BrainStructure.objects.all(), to_field_name="latin_name",
+        queryset=BrainStructure.objects.order_by('latin_name'), to_field_name="latin_name",
         widget=forms.RadioSelect, empty_label=None
     )
 
