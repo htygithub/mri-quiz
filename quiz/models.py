9ef348bf5095bd5c0834c9cdab0881854069e64c
Lucas van Dijk <info@return1.net>
2014-06-24 21:08:36 +0200

    Added some models for general multiple choice quizes

--- quiz/models.py
+++ quiz/models.py
@@ -30,6 +30,9 @@ def generate_random_queue():
     return ids
 
 class BrainStructure(models.Model):
+    """
+        Quiz model for MRI images
+    """
     latin_name = models.CharField(max_length=200)
     english_name = models.CharField(max_length=200)
     description = models.TextField()
@@ -38,7 +41,35 @@ class BrainStructure(models.Model):
         return "{} ({})".format(self.latin_name, self.english_name)
 
 class MRISet(models.Model):
+    """
+        Sets of MRI images which correspond to a certain brain structure
+    """
     structure = models.ForeignKey(BrainStructure, related_name="mri_sets")
     image1 = models.ImageField(upload_to=gen_filename)
     image2 = models.ImageField(upload_to=gen_filename)
     image3 = models.ImageField(upload_to=gen_filename)
+
+class Group(models.Model):
+    """
+        A group which holds multiple quizes
+    """
+
+    name = models.CharField(max_length=200)
+
+class Quiz(models.Model):
+    """
+        General multiple choice quiz
+    """
+
+    group = models.ForeignKey(Group, related_name="quizes")
+    question = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=gen_filename, blank=True)
+
+class Answers(models.Model):
+    """
+        Possible answers for a quiz
+    """
+
+    quiz = models.ForeignKey(Quiz, related_name="answers")
+    answer = models.CharField(max_length=255)
+
