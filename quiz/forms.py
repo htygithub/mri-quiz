767ac401a8b9faf2c939bf4a292d05be12970608
Lucas van Dijk <info@return1.net>
2014-06-24 21:24:53 +0200

    Added function to generate quiz form

--- quiz/forms.py
+++ quiz/forms.py
@@ -2,10 +2,19 @@ from django import forms
 
 from quiz.models import BrainStructure
 
-class QuizSubmitForm(forms.Form):
+class MRIQuizSubmitForm(forms.Form):
     answer = forms.ModelChoiceField(
         queryset=BrainStructure.objects.order_by('latin_name'), to_field_name="latin_name",
         widget=forms.RadioSelect, empty_label=None
     )
 
+def create_quiz_form(quiz):
+    class QuizSubmitForm(forms.Form):
+        answer = forms.ModelChoiceField(
+            queryset=quiz.answers.order_by('?'),
+            widget=forms.RadioSelect,
+            empty_label=None
+        )
+
+    return QuizSubmitForm
 
